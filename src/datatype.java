71fabba3deefc404c966fe8714a5ba5b8dc23e27
95suskjg <95suskjg@naver.com>
2022-06-07 09:47:14 +0900

    first commit

---
+++ src/datatype.java
@@ -0,0 +1,18 @@
+
+public class datatype {
+    public static void main(String[] args){
+        System.out.println(6); // Number 숫자
+        System.out.println("six"); // String 문자열
+
+        System.out.println("6"); //String 6 문자열인 숫자
+
+        System.out.println(6+6); //결과 12
+
+        System.out.println("6"+"6"); //결과 66 여기에서 +는 결합연산자
+
+        System.out.println(6*6); //36
+
+        System.out.println("1111".length()); //
+
+    }
+}
---
+++ src/lifecodingstrings.java
@@ -0,0 +1,9 @@
+public class lifecodingstrings {
+    public static void main(String[] args) {
+        String name = "riza";
+        System.out.println("Hello, "+name+" ... "+name+" ... egoing ... bye");
+
+        double VAT = 10.0;
+        System.out.println(VAT);
+    }
+}
