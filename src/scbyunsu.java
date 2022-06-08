7864bbef6cf0447e7b39c42ba78f845810a23f1b
95suskjg <95suskjg@naver.com>
2022-06-08 16:01:29 +0900

    second commit

---
+++ src/scbyunsu.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class scbyunsu {
+    public static void main (String [] args){
+        System.out.printf("age:%d year:%d\n", 14, 2017);
+        System.out.printf("%d\n", 15);
+        System.out.printf("%o\n", 15);
+        System.out.printf("%x\n", 15);
+        System.out.printf("%s\n", Integer.toBinaryString(15));
+
+         float f = 123.4567890f;
+        System.out.printf("%f\n", f);
+        System.out.printf("%g\n", 0.0000000001);
+        
+        Scanner scanner = new Scanner(System.in);
+    }
+}
