e8534b1b149f8e1f993b6942eafe89ab2b697c8b
95suskjg <95suskjg@naver.com>
2022-06-14 19:36:50 +0900

    20220614

---
+++ src/해시셋연습.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class 해시셋연습 {
+    public static void main(String[] args){
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+
+        System.out.println(set);
+    }
+}
