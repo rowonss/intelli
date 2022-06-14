6e136163a05d7a639a7f79f5ce5d79889caa5c91
95suskjg <95suskjg@naver.com>
2022-06-14 23:49:05 +0900

    20220614

--- src/javabasic.java
+++ src/javabasic.java
@@ -1,5 +1,8 @@
 public class javabasic {
     public static void main(String[] args){
         System.out.println("hello");
+        int age ;
+        age = 20;
+        System.out.println(age);
     }
 }
