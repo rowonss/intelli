9874703645797fb37a99e9f18c4e9606476fb289
95suskjg <95suskjg@naver.com>
2022-06-15 11:26:33 +0900

    20220615

--- src/소수구하기.java
+++ src/소수구하기.java
@@ -4,40 +4,32 @@ public class 소수구하기 {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("소수를 구하는 배열에 넣은 수자를 순서대로 넣어주세요");
+        System.out.println("숫자를 넣어주세요");
         int numbers = scanner.nextInt();
-
-        int[] select = int()
-
-        int[] num = {};
-        int answer = Solution2.solution(num);
+        int num = numbers;
+        String answer = Solution2.solution(num);
         System.out.println(answer);
-    }
-    class Solution2 {
-        public static int solution(int[] nums) {
-            int answer = 0;
-
-            for (int fir = 0; fir < nums.length - 2; fir++) {
-                for (int sec = fir + 1; sec < nums.length - 1; sec++) {
-                    for (int thr = sec + 1; thr < nums.length; thr++) {
-                        int count =0;
-                        int sum = nums[fir] + nums[sec] + nums[thr];
-
-                        for(int k=1; k<=sum; k++){
-                            if(sum%k==0){
-                                count+=1;
-                            }
-                        }
-                        if(count==2){
-                            answer+=1;
-                        }
-                        count=0;
-                    }
-
-                }
-            }
-            return answer;
 
         }
     }
-}
+    class Solution2 {
+        public static String solution(int nums) {
+            Boolean answer = true;
+            String tr = "소수입니다";
+            String fa = "소수가 아닙니다";
+            int count = 0;
+            for (int i = 1; i <= nums; i++) {
+
+                if (nums % i == 0) {
+                    count += 1;}
+
+            }
+            if (count == 2) {
+                return tr;
+            } else {
+                return fa;
+            }
+        }
+    }
+
+
