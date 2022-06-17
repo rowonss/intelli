dd4630eba9dbe9bc97eb56e062982435a196348b
95suskjg <95suskjg@naver.com>
2022-06-17 16:57:08 +0900

    20220617

---
+++ src/study/Client.java
@@ -0,0 +1,16 @@
+package study;
+
+public class Client {
+    public static void main(String[] args) {
+        Animal monkey = new Animal("원숭이");
+        Animal tiger = new Animal("코끼리");
+        tiger.setName("호랑이");
+        System.out.println(monkey.getName());
+
+
+        cass beer = new cass("맥주");
+
+        System.out.println();
+    }
+}
+
---
+++ src/study/cass.java
@@ -0,0 +1,10 @@
+package study;
+
+public class cass {
+
+    private String name;
+
+    public cass(String name){
+        this.name = name;
+    }
+}
