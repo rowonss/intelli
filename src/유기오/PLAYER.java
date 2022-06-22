ab49af80d8a6477eafab3820a337ec27e5147f84
95suskjg <95suskjg@naver.com>
2022-06-22 17:27:38 +0900

    20220622

--- src/유기오/PLAYER.java
+++ src/유기오/PLAYER.java
@@ -1,4 +1,16 @@
 package 유기오;
 
 public class PLAYER {
+    public static void main(String[] args) {
+
+        int monster = 0;
+
+        int yong = 20;
+
+        monster = (int)(Math.random()*10)+yong;
+
+        int str = (int)(Math.random()*50)+yong;
+
+        System.out.println(monster);
+    }
 }
