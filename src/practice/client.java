58a801b0c73a8a70615b2df5351e62b9b12c449a
95suskjg <95suskjg@naver.com>
2022-06-23 14:14:23 +0900

    20220623

--- src/practice/client.java
+++ src/practice/client.java
@@ -7,6 +7,7 @@ public class client {
     public static int money = 0;
 
     public static void main(String[] args) {
+        machine set = new machine();
 
         user();
 
@@ -14,8 +15,6 @@ public class client {
 
 
     public static void user() {
-
-        machine set = new machine();
 
         Scanner sc = new Scanner(System.in);
         System.out.println("1. 메뉴 목록 " + "2. 음료 구매" + "3. 메뉴 추가"  + "4. 금액 투입");
@@ -84,10 +83,30 @@ public class client {
         Scanner sc = new Scanner(System.in);
         System.out.println("투입된 금액 : "+money+"원");
         System.out.println("구매하실 상품의 번호를 선택해주세요");
+        System.out.println("구매를 취소하시려면 0을 눌러주세요");
         for (int i = 0; i < machine.drinklist.size(); i++) {
             System.out.println(i + 1 + "." + machine.drinklist.get(i)+" "+machine.drinkprice.get(i)+"원");
         }
         int num = sc.nextInt();
+        if(num==0){
+            user();
+        }
+        if(money<machine.drinkprice.get(num-1)){
+            System.out.println("잔액이 부족합니다");
+            buy();
+        }
+        System.out.println(machine.drinklist.get(num-1)+"이(가) 나왔습니다");
+        money -= machine.drinkprice.get(num-1);
+        System.out.println("남은 금액은 "+money+"원 입니다");
+        System.out.println("추가로 구입하시려면 1을");
+        System.out.println("메뉴로 돌아가시려면 0을 입력 해주세요");
+        int re = sc.nextInt();
+        if (re == 0) {
+            user();
+        } else if (re == 1) {
+            buy();
+        }
+
 
 
     }
---
+++ src/휴먼자바/어노테이션.java
@@ -0,0 +1,7 @@
+package 휴먼자바;
+
+public class 어노테이션 {
+    public static void main(String[] args) {
+        System.out.println("testCode Start");
+    }
+}
