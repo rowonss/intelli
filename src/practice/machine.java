0120344b2b77b84d56f37562bebab7a3dca4ce52
95suskjg <95suskjg@naver.com>
2022-06-23 11:16:55 +0900

    20220623

--- src/practice/machine.java
+++ src/practice/machine.java
@@ -1,5 +1,18 @@
 package practice;
 
+import java.util.ArrayList;
+
 public class machine {
+
+    public static ArrayList<String> drinklist = new ArrayList<>();
+
+    private String name;
+    private int price;
+
+    machine(String name, int price){
+        this.name = name;
+        this.price = price;
+        drinklist.add(name);
+    }
 
 }
