ff0d93fbb41dfda2866ddea12376a3d1890174f1
95suskjg <95suskjg@naver.com>
2022-06-30 09:15:16 +0900

    20220630

---
+++ src/개인공부/싱글턴패턴/program.java
@@ -0,0 +1,9 @@
+package 개인공부.싱글턴패턴;
+
+public class program {
+
+    public static void main(String[] args) {
+        new FIRST().setAndPrintSettings();
+        new SECOND().printSettings();
+    }
+}
