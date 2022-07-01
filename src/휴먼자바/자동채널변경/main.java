03910688b3e2a36acc989d10fe7b70833b3d0f2f
95suskjg <95suskjg@naver.com>
2022-07-01 21:40:10 +0900

    20220701

---
+++ src/휴먼자바/자동채널변경/main.java
@@ -0,0 +1,21 @@
+package 휴먼자바.자동채널변경;
+
+import java.util.Scanner;
+
+
+public class main extends 채널목록{
+
+    public int 채널 = 0;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        채널목록 list = new 채널목록();
+
+        System.out.println("tv 전원을 켭니다");
+        System.out.println("원하시는 채널 번호를 입력 해주세요");
+        int num = sc.nextInt();
+        chanchan(num);
+
+    }
+}
---
+++ src/휴먼자바/자동채널변경/searchable.java
@@ -0,0 +1,17 @@
+package 휴먼자바.자동채널변경;
+
+interface searchable {
+
+    int[] channel = {10, 20, 25, 120};
+
+    static int change(int x){
+        int k=0;
+        for(int i=0; i<channel.length; i++){
+            if(Math.abs(k-x)>Math.abs(channel[i]-x)){
+                k=channel[i];
+            }
+        }
+        return k;
+    }
+
+}
