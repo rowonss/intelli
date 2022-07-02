2184b0b77337f53580e6a418b5fe510ecc934ec7
95suskjg <95suskjg@naver.com>
2022-07-02 11:00:00 +0000

    20220701

---
+++ src/휴먼자바/자동채널변경/양호열.java
@@ -0,0 +1,4 @@
+package 휴먼자바.자동채널변경;
+
+public class 양호열 {
+}
---
+++ src/휴먼자바/자동채널변경/채널목록.java
@@ -0,0 +1,27 @@
+package 휴먼자바.자동채널변경;
+
+import static 휴먼자바.자동채널변경.searchable.change;
+
+public class 채널목록 implements searchable{
+
+    void 기본 (){};
+
+    static void chanchan (int x){
+        int ch = change(x);
+        if(ch == 10){
+            System.out.println("스포츠채널입니다");
+        }
+        else if(ch == 20){
+            System.out.println("게임채널입니다");
+        }
+        else if(ch == 25){
+            System.out.println("드라마채널입니다");
+        }
+        else if(ch == 120){
+            System.out.println("영화채널입니다");
+        }
+
+    }
+
+
+}
