8b13721bfaf6e235921649e3bc5bf4fa19385480
95suskjg <95suskjg@naver.com>
2022-07-08 09:08:58 +0900

    20220708

---
+++ src/휴먼자바/트라캣/ㅁㄴㅇ.java
@@ -0,0 +1,21 @@
+package 휴먼자바.트라캣;
+
+public class ㅁㄴㅇ {
+    public static void main(String[] args) {
+
+        int[] score = {10,30,20};
+
+        try{
+            System.out.println(score[3]);
+        }
+        catch (ArrayIndexOutOfBoundsException z){
+            System.out.println("ㄴㄴ");
+            System.out.println(z);
+        }
+
+
+
+
+
+    }
+}
