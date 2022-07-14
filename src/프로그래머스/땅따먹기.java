3f44cfe103d0f690a073313772b8b3ce00b6b6e7
95suskjg <95suskjg@naver.com>
2022-07-14 16:13:06 +0900

    20220714

---
+++ src/프로그래머스/땅따먹기.java
@@ -0,0 +1,37 @@
+package 프로그래머스;
+
+public class 땅따먹기 {
+    public static void main(String[] args) {
+
+        int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+
+        dkdk(land);
+
+    }
+
+    static void dkdk (int[][] land){
+
+        int answer = 0;
+
+        int index = 1000000;
+
+        for(int i=0; i<land.length; i++){
+            int max = 0;
+            int maxindex = 0;
+            for(int j=0; j<land[i].length;j++){
+                if(index == j ){
+                    continue;
+                }
+                if(land[i][j]>max){
+                    max = land[i][j];
+                    maxindex = j;
+                }
+            }
+            answer+=land[i][maxindex];
+            index = maxindex;
+        }
+
+        System.out.println(answer);
+
+    }
+}
