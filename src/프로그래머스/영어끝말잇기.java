c89fad944e3563be0ec7c9ecd242d9d4cb8c3727
95suskjg <95suskjg@naver.com>
2022-07-14 15:25:04 +0900

    20220714

--- src/프로그래머스/영어끝말잇기.java
+++ src/프로그래머스/영어끝말잇기.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class 영어끝말잇기 {
     public static void main(String[] args) {
-        String[] words = {"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"};
-        int n = 5;
+        String[] words = {"hello", "one", "even", "never", "now", "world", "draw"};
+        int n = 2;
 
         answer(n, words);
 
@@ -13,24 +13,27 @@ public class 영어끝말잇기 {
 
     static void answer(int n, String[] words) {
 
-        int index = 0;
-
         ArrayList<String> word = new ArrayList<>();
+
+        boolean yeah = true;
 
         for (int i = 0; i < words.length; i++) {
 
             if (word.contains(words[i])){
                 word.add(words[i]);
-                index = i+1;
+                yeah = false;
                 break;
             }
 
-            if (words[i].charAt(words[i].length() - 1) != words[i + 1].charAt(0)) {
-                word.add(words[i]);
-                word.add(words[i+1]);
-                index = i+2;
-                break;
+            else if(i<words.length-1){
+                if (words[i].charAt(words[i].length() - 1) != words[i + 1].charAt(0)) {
+                    word.add(words[i]);
+                    word.add(words[i+1]);
+                    yeah = false;
+                    break;
+                }
             }
+
             word.add(words[i]);
         }
 
@@ -45,8 +48,17 @@ public class 영어끝말잇기 {
             result += 1;
         }
 
+        if(yeah){
+            result = 0;
+            result2 = 0;
+        }
+
         System.out.println(result);
         System.out.println(result2);
+
+
+
+
 
 
 
