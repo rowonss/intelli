18b206091eda592522561a8d9c0ac8cb38454c90
95suskjg <95suskjg@naver.com>
2022-07-14 09:07:26 +0900

    20220714

---
+++ src/프로그래머스/문자열압축.java
@@ -0,0 +1,13 @@
+package 프로그래머스;
+
+public class 문자열압축 {
+    public static void main(String[] args) {
+
+        String a = "abcdefg";
+
+        System.out.println(a.substring(0,2));
+
+        System.out.println(a.length());
+
+    }
+}
