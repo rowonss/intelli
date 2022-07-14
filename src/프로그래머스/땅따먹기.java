73ed877c6c47c8fad7402517a365dfcc263794fc
95suskjg <95suskjg@naver.com>
2022-07-14 17:00:57 +0900

    20220714

--- src/프로그래머스/땅따먹기.java
+++ src/프로그래머스/땅따먹기.java
@@ -1,5 +1,7 @@
 package 프로그래머스;
 
+import java.util.ArrayList;
+
 public class 땅따먹기 {
     public static void main(String[] args) {
 
@@ -16,25 +18,45 @@ public class 땅따먹기 {
         int index = 1000000;
 
         for(int i=0; i<land.length; i++){
+            ArrayList<Integer> numbers1 = new ArrayList<>();
+            ArrayList<Integer> numbers2 = new ArrayList<>();
+
+            int nextlinemax = 0;
+            int nextlinesecondmax = 0;
+
             int max = 0;
+            int secondmax = 0;
             int maxindex = 0;
+
             for(int j=0; j<land[i].length;j++){
+
                 if(index == j ){
                     continue;
                 }
                 if(land[i][j]>max){
                     int x = 0;
-
-
-
-
+                    numbers1.add(land[i][j]);
                     max = land[i][j];
-                    System.out.println(max);
                     maxindex = j;
-
                 }
             }
+            secondmax = numbers1.get(numbers1.size()-2);
+            System.out.println(secondmax);
 
+            for(int j=0; j<land[i].length;j++){
+
+                if(land[i+1][j]>nextlinemax){
+                    numbers2.add(land[i+1][j]);
+                    nextlinemax = land[i+1][j];
+                }
+            }
+            System.out.println(numbers2);
+            nextlinesecondmax = numbers2.get(numbers2.size()-2);
+
+            System.out.println(nextlinemax);
+
+            int x = 0;
+            int y = 0;
 
 
 
