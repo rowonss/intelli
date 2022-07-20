6a6c2b1155a21c1988dfe1f0bb216f6830cf6a72
95suskjg <95suskjg@naver.com>
2022-07-21 08:53:08 +0900

    20220721

---
+++ src/JAVA_Basic2/basic_32_Object클래스.java
@@ -0,0 +1,15 @@
+package JAVA_Basic2;
+
+public class basic_32_Object클래스 {
+    public static void main(String[] args) {
+
+        hi h = new hi();
+
+        System.out.println(h.getClass());
+
+    }
+}
+
+class hi {
+
+}
