f83310ab5e028c68c7588c21b663b6f6be5d11ea
95suskjg <95suskjg@naver.com>
2022-07-21 16:03:55 +0900

    20220721

---
+++ src/휴먼자바/테스트.java
@@ -0,0 +1,8 @@
+package 휴먼자바;
+
+public class 테스트 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("프로그램이 정상적으로 실행됩니다 김종기");
+    }
+}
