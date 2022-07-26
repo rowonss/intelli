22d9f0a5360b3d0fba16317a3adcac594e6114c6
95suskjg <95suskjg@naver.com>
2022-07-26 09:08:23 +0900

    20220726

---
+++ src/백준/백준7_주어진수더하기.java
@@ -0,0 +1,22 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class 백준7_주어진수더하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        StringBuffer asd = new StringBuffer();
+
+        for(int i=0; i<a; i++){
+            int z = Integer.parseInt(br.readLine())+ Integer.parseInt(br.readLine());
+
+        }
+
+    }
+}
--- src/백준/백준8_주어진수까지합.java
+++ src/백준/백준8_주어진수까지합.java
@@ -2,14 +2,18 @@ package 백준;
 
 import java.util.Scanner;
 
-public class 백준6_구구단 {
+public class 백준8_주어진수까지합 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
 
-        for(int i=1; i<=9; i++){
-            System.out.println(a + " * " + i + " = "+ a*i );
+        int answer = 0;
+
+        for(int i=0; i<=a; i++){
+            answer += i;
         }
+
+        System.out.println(answer);
     }
 }
