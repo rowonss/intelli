11a337088d51ecba670db962072a092646c501ec
95suskjg <95suskjg@naver.com>
2022-07-31 20:02:42 +0900

    20220731

--- src/백준/백준7_주어진수더하기.java
+++ src/백준/백준7_주어진수더하기.java
@@ -3,6 +3,7 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class 백준7_주어진수더하기 {
@@ -22,6 +23,22 @@ public class 백준7_주어진수더하기 {
             int z = Integer.parseInt(br.readLine())+ Integer.parseInt(br.readLine());
 
         }
+
+    }
+
+    public String solution(String phone_number) {
+
+        String a = phone_number;
+        StringBuilder answer = new StringBuilder();
+
+        for(int i =0; i<a.length()-4; i++){
+            answer.append("*");
+        }
+
+        for(int i=a.length()-4; ; i++){
+            answer.append(a.charAt(i));
+        }
+
 
     }
 }
