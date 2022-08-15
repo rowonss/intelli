69ec2328be40bff19060c52ef0d476341ce73ddd
95suskjg <95suskjg@naver.com>
2022-08-16 08:41:12 +0900

    20220816

--- src/백준/Main.java
+++ src/백준/Main.java
@@ -29,3 +29,15 @@ public class Main {
 
     }
 }
+
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int a = Integer.parseInt(br.readLine());
+//
+//
+//        zxc.close();
+//
+//    }
+//}
