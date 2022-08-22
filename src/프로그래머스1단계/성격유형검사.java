5145a39835790390bc5aef61dcf2785123d3e658
95suskjg <95suskjg@naver.com>
2022-08-22 14:42:29 +0900

    20220822

--- src/프로그래머스1단계/성격유형검사.java
+++ src/프로그래머스1단계/성격유형검사.java
@@ -26,12 +26,6 @@ public class 성격유형검사 {
 
         StringBuilder asdzxc = new StringBuilder();
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-
-
         int[] zxc = new int[survey.length];
 
         for(int i=0; i<survey.length; i++){
