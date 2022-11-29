9abd4426c5b74c92f0a733ae72afb87b1a5d3883
rowonss <95suskjg@naver.com>
2022-11-29 12:44:46 +0900

    20221129

--- src/class20220613.java
+++ src/class20220613.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class class20220613 {
     public static void main(String[] args){
 
+
         ArrayList<String> inputArray = new ArrayList<String>();
         inputArray.add("Jane");
         inputArray.add("kim");
